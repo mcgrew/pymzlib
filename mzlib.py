bc7f42a2914e96b50d06cbc719c7e64f170c8ccd
Thomas McGrew <tjmcgrew@gmail.com>
2011-03-14 11:08:08 -0400

    Removed the 'Parser' class and moved all methods to functions, as they didn't depend on any functionality of the object anyway.

--- mzlib.py
+++ mzlib.py
@@ -38,288 +38,286 @@ import re
 import zlib
 import json
 
-class Parser( object ):
+#def read( filename ):
+#	"""
+#	Load a file into this reference. This method will automatically detect the
+#	file type based on the file extension.
+#
+#	:Parameters:
+#		filename : str
+#			The name of the file to load.
+#
+#	"""
+#
+#	if not os.path.exists( filename ):
+#		raise IOError( "The file %s does not exist or is not readable" % filename )
+#
+#	try:
+#		fileExt = filename[ filename.rindex( '.' )+1: ]
+#	except ValueError:
+#		return False
+#
+#	if ( fileExt.lower( ) == "csv" ):
+#		return readCsv( filename )
+#
+#	elif ( fileExt.lower( ) == "mzdata" ):
+#		return readMzData( filename )
+#
+#	elif ( fileExt.lower( ) == "mzxml" ):
+#		return readMzXml( filename )
+#	else:
+#		sys.stderr.write( "Unrecognized file type for %s\n" % filename )
+#		return False
+#
+def readCsv( filename ):
+	"""
+	Read a file in Agilent csv format. 
 
-	def __init__( self, options=None ):
-		"""
-		Initializes a MzData object.
+	:Parameters:
+		filename : str
+			The name of the file to load.
 
-		:Parameters:
-			options : None
-				This argument is deprecated and not used. Only here for compatibility.
+	"""
+	returnValue = { "scans" : [] }
+	try:
+		f = open( filename )
+		lines = f.readlines( )
+		f.close
+	except IOError:
+		sys.stderr.write( "Error: unable to read file '%s'\n" % filename )
+		return False
 
-		"""
-		object.__init__( self )
-
-	def read( self, filename ):
-		"""
-		Load a file into this reference. This method will automatically detect the
-		file type based on the file extension.
-
-		:Parameters:
-			filename : str
-				The name of the file to load.
-
-		"""
-
-		if not os.path.exists( filename ):
-			raise IOError( "The file %s does not exist or is not readable" % filename )
-
-		try:
-			fileExt = filename[ filename.rindex( '.' )+1: ]
-		except ValueError:
-			return False
-
-		if ( fileExt.lower( ) == "csv" ):
-			return self.readCsv( filename )
-
-		elif ( fileExt.lower( ) == "mzdata" ):
-			return self.readMzData( filename )
-
-		elif ( fileExt.lower( ) == "mzxml" ):
-			return self.readMzXml( filename )
-		else:
-			sys.stderr.write( "Unrecognized file type for %s\n" % filename )
-			return False
-
-	def readCsv( self, filename ):
-		"""
-		Read a file in Agilent csv format. You should only use this method if you
-		know the data is in this format and does not have the .csv extension.
-
-		:Parameters:
-			filename : str
-				The name of the file to load.
-
-		"""
-		returnValue = { "scans" : [] }
-		try:
-			f = open( filename )
-			lines = f.readlines( )
-			f.close
-		except IOError:
-			sys.stderr.write( "Error: unable to read file '%s'\n" % filename )
-			return False
-	
-		i = 0
-		while( i < len( lines ) and lines[ i ][ :10 ] != "file name," ):
-			i+= 1
-		returnValue[ 'sourceFile' ] = lines[ i ].split( ',' )[ 1 ]
-		while ( i < len( lines ) and lines[ i ][ :9 ] != "[spectra]" ):
-			i+=1
+	i = 0
+	while( i < len( lines ) and lines[ i ][ :10 ] != "file name," ):
+		i+= 1
+	returnValue[ 'sourceFile' ] = lines[ i ].split( ',' )[ 1 ]
+	while ( i < len( lines ) and lines[ i ][ :9 ] != "[spectra]" ):
 		i+=1
-	
-		if ( i > len( lines ) ):
-			sys.stderr.write( "Unable to parse the reference file '%s'\n" % filename ) 
-			return False
-	
-		scanId = 0
-		for line in lines[ i: ]:
-			scanId += 1
-			values = line.split( ',' )
-			if values[ 4 ] == '-':
-				polarity = -1
-			else:
-				polarity = 1
-			rt = float( values[ 0 ])
-			count = float( values[ 6 ])
-			intensityValues = [  float( x )  for x in values[ 8:-1:2 ] ]
-			massValues = [ float( y ) for y in values[ 7:-1:2 ] ]
+	i+=1
 
-			returnValue[ "scans" ].append({ 
-				"retentionTime" : rt,
-				"polarity" : polarity, 
-				"msLevel" : 1,
-				"id" : scanId,
-				"lowMz" : min( massValues ),
-				"highMz" : max( massValues ),
-				"parentScan" : None,
-				"precursorMz" : None,
-				"collisionEnergy" : None,
-				"data" : zip( massValues, intensityValues )
-			})
+	if ( i > len( lines ) ):
+		sys.stderr.write( "Unable to parse the reference file '%s'\n" % filename ) 
+		return False
 
-		return returnValue
+	scanId = 0
+	for line in lines[ i: ]:
+		scanId += 1
+		values = line.split( ',' )
+		if values[ 4 ] == '-':
+			polarity = -1
+		else:
+			polarity = 1
+		rt = float( values[ 0 ])
+		count = float( values[ 6 ])
+		intensityValues = [  float( x )  for x in values[ 8:-1:2 ] ]
+		massValues = [ float( y ) for y in values[ 7:-1:2 ] ]
 
-	def readMzData( self, filename ):
-		"""
-		Read a file in mzData format. You should only use this method if you
-		know the data is in this format and does not have the .mzdata extension.
+		returnValue[ "scans" ].append({ 
+			"retentionTime" : rt,
+			"polarity" : polarity, 
+			"msLevel" : 1,
+			"id" : scanId,
+			"lowMz" : min( massValues ),
+			"highMz" : max( massValues ),
+			"parentScan" : None,
+			"precursorMz" : None,
+			"collisionEnergy" : None,
+			"data" : zip( massValues, intensityValues )
+		})
 
-		:Parameters:
-			filename : str
-				The name of the file to load.
+	return returnValue
 
-		"""
-		returnValue = { "scans" : [] }
-		dataFile = parse( filename )
-		sourceFileNode = dataFile.getElementsByTagName( 'sourceFile' )[ 0 ].\
-			getElementsByTagName( 'nameOfFile' )[ 0 ]
-		sourceFile = re.sub( "<.*?>", "", sourceFileNode.toxml( ))
-		scans = dataFile.getElementsByTagName( 'spectrum' )
+def readMzData( filename ):
+	"""
+	Read a file in mzData format. 
 
-		for scan in scans:
-			parentScan = None
-			precursorMz = None
-			collisionEnergy = None
-			scanId = int( scan.getAttribute( 'id' ))
-			spectrumInstrument = scan.getElementsByTagName( 'spectrumInstrument' )[ 0 ]
-			msLevel = int( spectrumInstrument.getAttribute( 'msLevel' ))
-			lowMz = float( spectrumInstrument.getAttribute( 'mzRangeStart' ))
-			highMz = float( spectrumInstrument.getAttribute( 'mzRangeStop' ))
-			params = spectrumInstrument.getElementsByTagName( 'cvParam' )
-			for param in params:
-				if param.getAttribute( 'name' ) == 'Polarity':
-					if param.getAttribute( 'value' ) == 'positive':
-						polarity = 1
-					else:
-						polarity = -1
-				if param.getAttribute( 'name' ) == 'TimeInMinutes':
-					rt = float( param.getAttribute( 'value' ))
+	:Parameters:
+		filename : str
+			The name of the file to load.
 
-			
-			massValues = self._unpackMzData( 
-				scan.getElementsByTagName( 'mzArrayBinary' )[ 0 ].getElementsByTagName( 'data' )[ 0 ])
-			intensityValues = self._unpackMzData( 
-				scan.getElementsByTagName( 'intenArrayBinary' )[ 0 ].getElementsByTagName( 'data' )[ 0 ])
+	"""
+	returnValue = { "scans" : [] }
+	dataFile = parse( filename )
+	sourceFileNode = dataFile.getElementsByTagName( 'sourceFile' )[ 0 ].\
+		getElementsByTagName( 'nameOfFile' )[ 0 ]
+	sourceFile = re.sub( "<.*?>", "", sourceFileNode.toxml( ))
+	scans = dataFile.getElementsByTagName( 'spectrum' )
 
-			precursors = scan.getElementsByTagName( 'precursor' )
-			for precursor in precursors[ 0:1 ]:
-				parentScan = int( precursor.getAttribute( 'spectrumRef' ))
-				cvParams = precursor.getElementsByTagName( 'cvParam' )
-				for param in cvParams:
-					if param.getAttribute( 'name' ) == 'MassToChargeRatio':
-						precursorMz = float( param.getAttribute( 'value' ))
+	for scan in scans:
+		parentScan = None
+		precursorMz = None
+		collisionEnergy = None
+		scanId = int( scan.getAttribute( 'id' ))
+		spectrumInstrument = scan.getElementsByTagName( 'spectrumInstrument' )[ 0 ]
+		msLevel = int( spectrumInstrument.getAttribute( 'msLevel' ))
+		lowMz = float( spectrumInstrument.getAttribute( 'mzRangeStart' ))
+		highMz = float( spectrumInstrument.getAttribute( 'mzRangeStop' ))
+		params = spectrumInstrument.getElementsByTagName( 'cvParam' )
+		for param in params:
+			if param.getAttribute( 'name' ) == 'Polarity':
+				if param.getAttribute( 'value' ) == 'positive':
+					polarity = 1
+				else:
+					polarity = -1
+			if param.getAttribute( 'name' ) == 'TimeInMinutes':
+				rt = float( param.getAttribute( 'value' ))
+
+		
+		massValues = _unpackMzData( 
+			scan.getElementsByTagName( 'mzArrayBinary' )[ 0 ].getElementsByTagName( 'data' )[ 0 ])
+		intensityValues = _unpackMzData( 
+			scan.getElementsByTagName( 'intenArrayBinary' )[ 0 ].getElementsByTagName( 'data' )[ 0 ])
+
+		precursors = scan.getElementsByTagName( 'precursor' )
+		for precursor in precursors[ 0:1 ]:
+			parentScan = int( precursor.getAttribute( 'spectrumRef' ))
+			cvParams = precursor.getElementsByTagName( 'cvParam' )
+			for param in cvParams:
+				if param.getAttribute( 'name' ) == 'MassToChargeRatio':
+					precursorMz = float( param.getAttribute( 'value' ))
 #					if param.getAttribute( 'name' ) == 'ChargeState':
 #						chargeState = int( param.getAttribute( 'value' ))
-					if param.getAttribute( 'name' ) == 'CollisionEnergy':
-						collisionEnergy = float( param.getAttribute( 'value' ))
+				if param.getAttribute( 'name' ) == 'CollisionEnergy':
+					collisionEnergy = float( param.getAttribute( 'value' ))
 
 
 
-			returnValue[ "scans" ].append({ 
-				"retentionTime" : rt,
-				"polarity" : polarity, 
-				"msLevel" : msLevel, 
-				"id" : scanId,
-				"lowMz" : lowMz,
-				"highMz" : highMz,
-				"parentScan" : parentScan,
-				"precursorMz" : precursorMz,
-				"collisionEnergy" : collisionEnergy,
-				"data" : zip( massValues, intensityValues )
-			})
+		returnValue[ "scans" ].append({ 
+			"retentionTime" : rt,
+			"polarity" : polarity, 
+			"msLevel" : msLevel, 
+			"id" : scanId,
+			"lowMz" : lowMz,
+			"highMz" : highMz,
+			"parentScan" : parentScan,
+			"precursorMz" : precursorMz,
+			"collisionEnergy" : collisionEnergy,
+			"data" : zip( massValues, intensityValues )
+		})
 
-		returnValue[ 'sourceFile' ] = sourceFile
+	returnValue[ 'sourceFile' ] = sourceFile
 
-		return returnValue
+	return returnValue
 
-	def _unpackMzData( self, dataNode ):
-		"""
-		Internal function. Unpacks the scan data contained in a <data> node in mzdata 
-		format.
-	
-		:Parameters:
-			dataNode : xmlNode
-				The xml node containing the scan data to be unpacked.
+def _unpackMzData( dataNode ):
+	"""
+	Internal function. Unpacks the scan data contained in a <data> node in mzdata 
+	format.
 
-		"""
-		scanSize = int( dataNode.getAttribute( 'length' ))
-		if dataNode.getAttribute( 'endian' ) == 'little':
-			byteOrder = '<'
+	:Parameters:
+		dataNode : xmlNode
+			The xml node containing the scan data to be unpacked.
+
+	"""
+	scanSize = int( dataNode.getAttribute( 'length' ))
+	if dataNode.getAttribute( 'endian' ) == 'little':
+		byteOrder = '<'
+	else:
+		byteOrder = '>'
+	if dataNode.getAttribute( 'precision' ) == '64':
+		dataType = 'd'
+	else: 
+		dataType = 'f'
+
+	return struct.unpack( byteOrder + ( dataType * scanSize ), 
+		b64decode( re.sub( "<.*?>", "", dataNode.toxml( ))))
+
+def readMzXml( filename ):
+	"""
+	Read a file in mzXML format.
+
+	:Parameters:
+		filename : str
+			The name of the file to load.
+
+	"""
+	returnValue = { "scans" : [] }
+	dataFile = parse( filename )
+	scans = dataFile.getElementsByTagName( 'scan' )
+	for scan in scans:
+		collisionEnergy = None
+		precursorMz = None
+		msLevel = int( scan.getAttribute( "msLevel" ))
+		scanSize = int( scan.getAttribute( 'peaksCount' ))
+		rt = float( scan.getAttribute( 'retentionTime' )[ 2:-1 ] ) / 60
+		scanId = int( scan.getAttribute( 'num' ))
+		lowMz = float( scan.getAttribute( 'lowMz' ))
+		highMz = float( scan.getAttribute( 'highMz' ))
+		if ( scan.getAttribute( 'polarity' ) == '+' ):
+			polarity = 1
 		else:
-			byteOrder = '>'
-		if dataNode.getAttribute( 'precision' ) == '64':
-			dataType = 'd'
-		else: 
-			dataType = 'f'
-
-		return struct.unpack( byteOrder + ( dataType * scanSize ), 
-			b64decode( re.sub( "<.*?>", "", dataNode.toxml( ))))
-
-	def readMzXml( self, filename ):
-		"""
-		Read a file in mzXML format. You should only use this method if you
-		know the data is in this format and does not have the .mzxml extension.
-
-		:Parameters:
-			filename : str
-				The name of the file to load.
-
-		"""
-		returnValue = { "scans" : [] }
-		dataFile = parse( filename )
-		scans = dataFile.getElementsByTagName( 'scan' )
-		for scan in scans:
-			collisionEnergy = None
-			precursorMz = None
-			msLevel = int( scan.getAttribute( "msLevel" ))
-			scanSize = int( scan.getAttribute( 'peaksCount' ))
-			rt = float( scan.getAttribute( 'retentionTime' )[ 2:-1 ] ) / 60
-			scanId = int( scan.getAttribute( 'num' ))
-			lowMz = float( scan.getAttribute( 'lowMz' ))
-			highMz = float( scan.getAttribute( 'highMz' ))
-			if ( scan.getAttribute( 'polarity' ) == '+' ):
-				polarity = 1
-			else:
-				polarity = -1
-			if msLevel == 1:
-				parentScan = None
-			else:
-				parentScan = int( scan.parentNode.getAttribute( 'num' ))
-				if ( scan.getAttribute( 'collisionEnergy' )):
-					collisionEnergy = float( scan.getAttribute( 'collisionEnergy' ))
-				precursorTags = scan.getElementsByTagName( 'precursorMz' )
-				if ( len( precursorTags )):
-					precursorMz = float( re.sub( r"<.*?>", "", precursorTags[ 0 ].toxml( )).strip( ))
-			
-
-			peaks = scan.firstChild
-			while not ( peaks.nodeType == peaks.ELEMENT_NODE and peaks.tagName == 'peaks' ):
-				peaks = peaks.nextSibling
-
-			if peaks.getAttribute( 'precision' ) == '64':
-				type = 'd'
-			else: 
-				type='f'
-			byteOrder = '>'
-
-			# get all of the text (non-tag) content of peaks
-			packedData = re.sub( r"<.*?>", "", peaks.toxml( )).strip( )
-			if ( peaks.getAttribute( 'compressionType' ) == 'zlib' ):
-				data = struct.unpack( byteOrder + ( type * scanSize * 2 ), zlib.decompress( b64decode( packedData )))
-			else:
-				data = struct.unpack( byteOrder + ( type * scanSize * 2 ), b64decode( packedData ))
-			massValues = data[ 0::2 ]
-			intensityValues = data[ 1::2 ] 
-
-			returnValue[ "scans" ].append({ 
-				"retentionTime" : rt,
-				"polarity" : polarity, 
-				"msLevel" : msLevel, 
-				"id" : scanId,
-				"lowMz" : lowMz,
-				"highMz" : highMz,
-				"parentScan" : parentScan,
-				"precursorMz" : precursorMz,
-				"collisionEnergy" : collisionEnergy,
-				"data" : zip( massValues, intensityValues )
-			})
-
-		return returnValue
-
-	def readMzml( self, filename ):
-		pass
+			polarity = -1
+		if msLevel == 1:
+			parentScan = None
+		else:
+			parentScan = int( scan.parentNode.getAttribute( 'num' ))
+			if ( scan.getAttribute( 'collisionEnergy' )):
+				collisionEnergy = float( scan.getAttribute( 'collisionEnergy' ))
+			precursorTags = scan.getElementsByTagName( 'precursorMz' )
+			if ( len( precursorTags )):
+				precursorMz = float( re.sub( r"<.*?>", "", precursorTags[ 0 ].toxml( )).strip( ))
 		
 
-	def _getChildNode( self, node, child ):
-		returnValue = node.firstChild
-		while returnValue and not ( 
-			returnValue.nodeType == returnValue.ELEMENT_NODE and 
-			returnValue.tagName == child ):
+		peaks = scan.firstChild
+		while not ( peaks.nodeType == peaks.ELEMENT_NODE and peaks.tagName == 'peaks' ):
+			peaks = peaks.nextSibling
 
-			returnValue = returnValue.nextSibling
-		return returnValue
+		if peaks.getAttribute( 'precision' ) == '64':
+			type = 'd'
+		else: 
+			type='f'
+		byteOrder = '>'
+
+		# get all of the text (non-tag) content of peaks
+		packedData = re.sub( r"<.*?>", "", peaks.toxml( )).strip( )
+		if ( peaks.getAttribute( 'compressionType' ) == 'zlib' ):
+			data = struct.unpack( byteOrder + ( type * scanSize * 2 ), zlib.decompress( b64decode( packedData )))
+		else:
+			data = struct.unpack( byteOrder + ( type * scanSize * 2 ), b64decode( packedData ))
+		massValues = data[ 0::2 ]
+		intensityValues = data[ 1::2 ] 
+
+		returnValue[ "scans" ].append({ 
+			"retentionTime" : rt,
+			"polarity" : polarity, 
+			"msLevel" : msLevel, 
+			"id" : scanId,
+			"lowMz" : lowMz,
+			"highMz" : highMz,
+			"parentScan" : parentScan,
+			"precursorMz" : precursorMz,
+			"collisionEnergy" : collisionEnergy,
+			"data" : zip( massValues, intensityValues )
+		})
+
+	return returnValue
+
+def readMzml( filename ):
+	pass
+	
+
+def _getChildNode( node, child ):
+	"""
+	Internal function. Finds the child node of the passed in xml node with the 
+	given tag name.
+
+	:Parameters:
+		node : minidom node
+			A minidom node object
+		child : str
+			A string containing the tag name of the child node to return.
+		
+
+	rtype: minidom node
+	return: The requested child of the minidom node.
+	"""
+	returnValue = node.firstChild
+	while returnValue and not ( 
+		returnValue.nodeType == returnValue.ELEMENT_NODE and 
+		returnValue.tagName == child ):
+
+		returnValue = returnValue.nextSibling
+	return returnValue
 
 def removeScans( mzData, minTime=0, maxTime=sys.maxint ):
 	"""
